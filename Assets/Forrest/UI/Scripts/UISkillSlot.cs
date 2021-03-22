bd04cbd0c31cdb6214ed2b06420498df5d871ef6
NouCake <KevinFischer2010@gmail.com>
2021-03-22 18:14:38 +0100

    Reorganized UI Assets

---
+++ Assets/Forrest/UI/Scripts/UISkillSlot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISkillSlot : MonoBehaviour {
+
+    [SerializeField] private TMP_Text SlotNumber;
+    [SerializeField] private Image SkillImage;
+    [SerializeField] private GameObject Selection;
+
+    private void Awake() {
+        SetSelected(false);
+    }
+
+    public void SetSelected(bool selected) {
+        Selection.SetActive(selected);
+    }
+
+    public void SetSlotNumber(int number) {
+        SlotNumber.text = number + "";
+    }
+
+    public void SetSkillImage(Sprite image) {
+        SkillImage.sprite = image;
+    }
+
+
+}
